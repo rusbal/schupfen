dc5eb193508f7317ad4866cb1b5c0a767faf0646
Raymond Usbal <raymond@philippinedev.com>
2016-06-12 16:30:23 +0800

    Developing Porto theme on Camaleon CMS

---
+++ app/apps/themes/porto/assets/js/main.js
@@ -0,0 +1,46 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+// require jquery
+// require camaleon_cms/bootstrap.min.js
+//
+//= require ./vendor/modernizr/modernizr.min.js
+//
+// -------- Vendor
+//= require ./vendor/jquery/jquery.min.js
+//= require ./vendor/jquery.appear/jquery.appear.min.js
+//= require ./vendor/jquery.easing/jquery.easing.min.js
+//= require ./vendor/jquery-cookie/jquery-cookie.min.js
+//= require ./vendor/bootstrap/js/bootstrap.min.js
+//= require ./vendor/common/common.min.js
+//= require ./vendor/jquery.validation/jquery.validation.min.js
+//= require ./vendor/jquery.stellar/jquery.stellar.min.js
+//= require ./vendor/jquery.easy-pie-chart/jquery.easy-pie-chart.min.js
+//= require ./vendor/jquery.gmap/jquery.gmap.min.js
+//= require ./vendor/jquery.lazyload/jquery.lazyload.min.js
+//= require ./vendor/isotope/jquery.isotope.min.js
+//= require ./vendor/owl.carousel/owl.carousel.min.js
+//= require ./vendor/magnific-popup/jquery.magnific-popup.min.js
+//= require ./vendor/vide/vide.min.js
+// 
+// -------- Theme Base, Components and Settings
+//= require ./theme.js
+// 
+// -------- Current Page Vendor and Views
+//= require ./vendor/rs-plugin/js/jquery.themepunch.tools.min.js
+//= require ./vendor/rs-plugin/js/jquery.themepunch.revolution.min.js
+// 
+// -------- Theme Custom
+//= require ./custom.js
+// 
+// -------- Theme Initialization Files
+//= require ./theme.init.js
